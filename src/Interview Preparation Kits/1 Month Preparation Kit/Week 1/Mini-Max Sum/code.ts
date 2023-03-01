111eefe50857b95f330ab805c1282571b895aab0
Daniel Parra <danielparra@encodingtech.com>
2023-03-01 00:27:01 -0400

    [ADD] Mini-Max Sum

---
+++ src/Interview Preparation Kits/1 Month Preparation Kit/Week 1/Mini-Max Sum/code.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr: number[]): void {
+    const sum = arr.reduce((a, b) => a + b)
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    console.log(`${sum - max} ${sum - min}`)
+}
+
+function main() {
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
