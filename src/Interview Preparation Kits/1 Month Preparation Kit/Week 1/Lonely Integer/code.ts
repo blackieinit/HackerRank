9b832a9b81a3d9e5314aa1e868e80653e93c9bf7
Daniel Parra <danielparra@encodingtech.com>
2023-03-04 21:56:22 -0400

    [ADD] Lonely Integer

---
+++ src/Interview Preparation Kits/1 Month Preparation Kit/Week 1/Lonely Integer/code.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a: number[]): number {
+    for (let i = 0; i <= a.length; i++) {
+        let count = 0
+        for (let z = 0; z <= a.length; z++) {
+            if (a[z] === a[i]) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            return a[i];
+        }
+    }
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const a: number[] = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result: number = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
